f52e865556e916f928e7f7a851db74a36b7e3f63
NPSS93 <npss.26051993@gmail.com>
2024-01-12 12:39:06 +0700

    first commit

--- index.js
+++ index.js
@@ -6,37 +6,21 @@ app.use(express.json());
 
 app.get('/color', async (req, res) => {
   try {
+    
     let id = req.query.id;
+    
     let imageUrl = "https://lh5.googleusercontent.com/d/" + String(id);
     console.log(imageUrl);
-
-    ColorThief.getColor(imageUrl)
-      .then(color => {
-        console.log(color);
-        const hexColor = rgbToHex(color[0], color[1], color[2]);
-        console.log(hexColor);
-
-        // Set background color and include image
-        res.status(200).send(`
-          <html>
-            <head>
-              <style>
-                body {
-                  background-color: ${hexColor};
-                }
-              </style>
-            </head>
-            <body>
-              <img src="${imageUrl}" alt="Image" width="300" height="200">
-              <p>Background color is set to ${hexColor}.</p>
-            </body>
-          </html>
-        `);
+    
+     ColorThief.getColor(imageUrl)
+     .then(color => 
+       {
+         console.log(color);
+         const hexColor = rgbToHex(color[0], color[1], color[2]);
+         console.log(hexColor);
+         res.status(200).json(hexColor);
       })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: 'An error occurred' });
-      });
+    .catch(err => { console.log(err) })
 
   } catch (error) {
     console.error('Error:', error);
